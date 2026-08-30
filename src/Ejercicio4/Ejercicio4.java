e7522fb471825f6dc82978242e621e1962dcd500
Gamehac_RA <gukura90@gmail.com>
2026-08-30 01:58:47 -0600

    Add Ejercicio4: Print numbers 1 to 100 using for loop

---
+++ src/Ejercicio4/Ejercicio4.java
@@ -0,0 +1,11 @@
+package Ejercicio4;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Sayori is the best btw
