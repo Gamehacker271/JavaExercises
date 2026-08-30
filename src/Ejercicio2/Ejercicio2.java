6b226929e80010c6ca3cb8aeba93e60fef6fdeb9
Gamehac_RA <gukura90@gmail.com>
2026-08-30 01:36:27 -0600

    Refactor Ejercicio2: Fix variable scope and add missing output requirements

--- src/Ejercicio2/Ejercicio2.java
+++ src/Ejercicio2/Ejercicio2.java
@@ -5,11 +5,12 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         boolean valid = false;
         double precio = 0.0;
+        double numero = 0.0;
+        final int iva = 21;
         do {
             try {
                 String input = JOptionPane.showInputDialog("Ingresa el precio del producto: ");
-                double numero = Double.parseDouble(input);
-                final int iva = 21;
+                numero = Double.parseDouble(input);
                 precio = numero + (numero * iva / 100);
                 valid = true;
             } catch (NumberFormatException e) {
@@ -17,7 +18,7 @@ public class Ejercicio2 {
             }
         } while (valid == false);
 
-        JOptionPane.showMessageDialog(null, "El precio del producto con IVA es: " + precio);
+        JOptionPane.showMessageDialog(null,"El precio original del producto fue: " + numero + "\n" + "El IVA aplicado fue del: " + iva + "\n" + "El precio del producto con IVA es: " + precio);
     }
 }
 // Sayori is the best btw
