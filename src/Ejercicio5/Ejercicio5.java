73b79e12dec00b8ddb1c8341ecea4b5cbaad123b
Gamehac_RA <gukura90@gmail.com>
2026-08-30 02:07:58 -0600

    Add Ejercicio5: Find numbers divisible by 2 and 3

---
+++ src/Ejercicio5/Ejercicio5.java
@@ -0,0 +1,15 @@
+package Ejercicio5;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println(i + " es divisible entre 2 y 3");
+            } else {
+                System.out.println(i + " no es divisible entre 2 y 3");
+            }
+        }
+    }
+}
+
+// Sayori is the best btw
