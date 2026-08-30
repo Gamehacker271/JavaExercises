8d14b950dc343c05aeca7821a3104280cd9c04d5
Gamehac_RA <gukura90@gmail.com>
2026-08-30 01:52:10 -0600

    Add Ejercicio3: Print numbers 1 to 100 using while loop

---
+++ src/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,13 @@
+package Ejercicio3;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int contador = 1;
+        while (contador <= 100) {
+            System.out.println(contador);
+            contador ++;
+        }
+    }
+}
+
+// Sayori is the best btw
